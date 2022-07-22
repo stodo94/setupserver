6825694a419aae32b551d25c5c5c1c6c5a874dbf
Stefan Becker <sb@seven20.de>
2022-07-22 11:54:04 +0200

    fixed 7zip Download

--- setupserver.ps1
+++ setupserver.ps1
@@ -243,7 +243,7 @@ function InstallDotNet48 {
 
 function Install7zip {
    InstallWGet
-   C:\Service\setupserver\bin\wget.exe https://7-zip.org/a/7z2201-x64.msi -q -P C:\Service\setupserver\bin --show-progress
+   C:\Service\setupserver\bin\wget.exe https://7-zip.org/a/7z2201-x64.msi -q -P C:\Service\setupserver\bin --show-progress  --no-check-certificate
    msiexec.exe /I C:\Service\setupserver\bin\7z2201-x64.msi /qn   
 }
 
